e383171807515be48f69f26e3b84b974cdc08c4d
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-14 16:13:46 +0200

    create references and object's array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
-    console.log($); //test
+    /*****************************
+     * TODO LIST
+     *****************************/
+
+    // REFERENCES
+    var input = $('.add');
+    var list = $('.todo-list');
+    var template = $('#template .todo-item');
+
+    // Object's Array
+    var items = [
+        {
+            text: 'Fare la spesa',
+            completed: false,
+        },
+        {
+            text: 'Fare la lavatrice',
+            completed: false,
+        },
+        {
+            text: 'Chiamare idraulico',
+            completed: false,
+        },
+    ];
     // End Doc Ready
 });
