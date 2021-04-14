180fa5dfe7781fe4f620e7c5eb17a50ce6f1c9e7
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-14 16:23:58 +0200

    populate todo list

--- js/script.js
+++ js/script.js
@@ -23,5 +23,17 @@ $(document).ready(function () {
             completed: false,
         },
     ];
+
+    // Insert Todo Items
+    for(var i = 0; i < items.length; i++){
+        // Template
+        var item = template.clone();
+
+        // Populate Template
+        item.children('.text').text(items[i].text);
+
+        // Print in HTML
+        list.append(item);
+    }
     // End Doc Ready
 });
